b27cfb3f5e63956ea4fc76dc7d2cf24ca680765d
root <root@DESKTOP-U1GEEL4>
2024-03-20 02:12:17 -0400

    added lenoir.py

---
+++ lenoir.py
@@ -0,0 +1,37 @@
+import json
+import logging
+from truthbrush import Api
+
+# Set up logging
+logging.basicConfig(filename='search_errors.log', level=logging.ERROR,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+api = Api()
+
+
+results = api.search(searchtype="statuses", query="Maga Hat")
+
+i = 0
+# Open a file for writing
+with open('corpus.json', 'w') as json_file:
+    # Write an opening bracket for the JSON array
+    json_file.write('[')
+
+    # Iterate over the results and write each one to the file
+    first = True
+    for result in results:
+        try:
+            print(
+                f"{i} | Lm: {api.ratelimit_remaining} | {api.ratelimit_reset}",
+                end="\r", flush=True)
+            i += 1
+            if not first:
+                json_file.write(',')
+            else:
+                first = False
+            json.dump(result, json_file, indent=4)
+        except Exception as e:
+            logging.error(
+                f"An error occurred while processing result {i}: {e}")
+    # Write a closing bracket for the JSON array
+    json_file.write(']')
